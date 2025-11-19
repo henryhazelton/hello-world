e3c971cd121e1754299f5a3750b22e31dd92d797
henryhazelton <henryhazelton00@gmail.com>
2025-11-19 20:14:15 +0000

    learning rust and its different for sure

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
-use rand::prelude::*;
+use rand::{prelude::*, random, rng};
 
 fn main() {
     println!("Hello, world!");
+
+    let mut rng = rand::rng();
+
+    let mut numbers: Vec<i32> = (1..100).collect();
+    let secret_number = numbers.choose(&mut rng);
+
+    println!("{}", secret_number);
+    
 }
